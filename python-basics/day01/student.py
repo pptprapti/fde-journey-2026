08f4b91fbcd482e049b61fe6d9d333e9249c1e08
Prapti Sinha <pptprapti@gmail.com>
2026-06-04 13:57:30 +0530

    Initial commit

---
+++ python-basics/day01/student.py
@@ -0,0 +1,5 @@
+name = "Prapti"
+experience = 4 
+
+print(f"My name is {name}")
+print(f"I have {experience} years of experience")
---
+++ python-basics/day01/validator.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age:"))
+
+if age>=18:
+    print("Elgible")
+else:
+    print("Not Eligible")
